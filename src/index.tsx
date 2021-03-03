6c36d4915257a1aa41de04f76016f40cd40d83a8
HimashiRathnayake <nikeshalarathnayake19@gmail.com>
2021-03-03 19:00:04 +0530

    Wrap root app component with redux store provider

--- src/index.tsx
+++ src/index.tsx
@@ -19,13 +19,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/app.less";
+import { Provider } from "react-redux";
 import { App } from "./app";
 import reportWebVitals from "./report-web-vitals";
+import { store } from "./store";
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={ store }>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById("root")
 );
 
